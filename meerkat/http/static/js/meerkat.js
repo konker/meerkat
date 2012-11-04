27fc2222d78f5533a535755350783bf9e0266743
konker <konker@gmail.com>
2012-11-05 00:37:58 +0200

    Add loading gif and refresh buttons

--- meerkat/http/static/js/meerkat.js
+++ meerkat/http/static/js/meerkat.js
@@ -10,54 +10,99 @@ var meerkat = (function($) {
             meerkat.probes.init();
             meerkat.log.init();
         },
+        loading: {
+            _stack: 0,
+            on: function() {
+                meerkat.loading._stack++;
+                meerkat.loading._setLoading(true);
+            },
+            off: function() {
+                meerkat.loading._stack--;
+                if (meerkat.loading._stack <= 0) {
+                    meerkat.loading._setLoading(false);
+                }
+            },
+            _setLoading: function(on) {
+                if (on) {
+                    $('h1').addClass('loading');
+                }
+                else {
+                    $('h1').removeClass('loading');
+                    meerkat.loading._stack = 0;
+                }
+            }
+        },
         master: {
             init: function() {
+                meerkat.loading.on();
                 meerkat.master.refresh();
             },
             masterToggle: function() {
+                /* TODO */
+                meerkat.loading.on();
+                meerkat.master.refresh();
+                return false;
+            },
+            masterRefresh: function() {
+                meerkat.loading.on();
                 meerkat.master.refresh();
                 return false;
             },
             refresh: function() {
                 /* fetch JSON data from the server */
-                $.getJSON('master.json', function(data, textStatus, jqXHR) {
-                    /*[TODO: error handling ]*/
-                    /* extra formatting */
-                    data.body.uptime_ms =
-                        meerkat.util.format_ms(data.body.uptime_ms);
-                    data.body.data_size_mb =
-                        meerkat.util.format_mb(data.body.data_size_mb);
-                    data.body.free_space_mb =
-                        meerkat.util.format_mb(data.body.free_space_mb);
+                $.ajax({
+                    url: 'master.json',
+                    dataType: 'json',
+                    error: function(data, textStatus, jqXHR) {
+                        /*[TODO: error handling ]*/
+                        alert(errorThrown);
+                        meerkat.loading.off();
+                    },
+                    success: function(data, textStatus, jqXHR) {
+                        /*[TODO: error handling ]*/
+                        /* extra formatting */
+                        data.body.uptime_ms =
+                            meerkat.util.format_ms(data.body.uptime_ms);
+                        data.body.data_size_mb =
+                            meerkat.util.format_mb(data.body.data_size_mb);
+                        data.body.free_space_mb =
+                            meerkat.util.format_mb(data.body.free_space_mb);
 
-                    /* render the data */
-                    $('#master').render(data.body, meerkat.master.directives.main);
+                        /* render the data */
+                        $('#master').render(data.body, meerkat.master.directives.main);
 
-                    /* event handlers */
-                    $('#masterToggle')
-                        .unbind('click')
-                        .bind('click', meerkat.master.masterToggle);
-
-                    /* visual aids */
-                    if (data.body.status == 'ON') {
+                        /* event handlers */
                         $('#masterToggle')
-                            .removeClass('btn-danger')
-                            .addClass('btn-success')
-                            .text('Master ON');
-                        $('#master dl')
-                            .find('dd.status')
-                            .removeClass('text-error')
-                            .addClass('text-success');
-                    }
-                    else {
-                        $('#masterToggle')
-                            .removeClass('btn-success')
-                            .addClass('btn-danger')
-                            .text('Master OFF');
-                        $('#master dl')
-                            .find('dd.status')
-                            .removeClass('text-success')
-                            .addClass('text-error');
+                            .unbind('click')
+                            .bind('click', meerkat.master.masterToggle);
+
+                        $('#masterRefresh')
+                            .unbind('click')
+                            .bind('click', meerkat.master.masterRefresh);
+
+                        /* visual aids */
+                        if (data.body.status == 'ON') {
+                            $('#masterToggle')
+                                .removeClass('btn-danger')
+                                .addClass('btn-success')
+                                .text('Master ON');
+                            $('#master dl')
+                                .find('dd.status')
+                                .removeClass('text-error')
+                                .addClass('text-success');
+                        }
+                        else {
+                            $('#masterToggle')
+                                .removeClass('btn-success')
+                                .addClass('btn-danger')
+                                .text('Master OFF');
+                            $('#master dl')
+                                .find('dd.status')
+                                .removeClass('text-success')
+                                .addClass('text-error');
+                        }
+
+                        meerkat.loading.off();
                     }
                 });
             },
@@ -76,9 +121,17 @@ var meerkat = (function($) {
             probes: null,
 
             init: function() {
+                meerkat.loading.on();
                 meerkat.probes.refresh();
             },
             probeToggle: function(e) {
+                /* TODO */
+                meerkat.loading.on();
+                meerkat.probes.refreshProbe(e.data.p);
+                return false;
+            },
+            probeRefresh: function(e) {
+                meerkat.loading.on();
                 meerkat.probes.refreshProbe(e.data.p);
                 return false;
             },
@@ -99,31 +152,63 @@ var meerkat = (function($) {
                 return false;
             },
             refresh: function() {
-                $.getJSON('probes.json', function(data, textStatus, jqXHR) {
-                    /*[TODO: error handling ]*/
+                $.ajax({
+                    url: 'probes.json',
+                    dataType: 'json',
+                    error: function(data, textStatus, jqXHR) {
+                        /*[TODO: error handling ]*/
+                        alert(errorThrown);
+                        meerkat.loading.off();
+                    },
+                    success: function(data, textStatus, jqXHR) {
+                        /*[TODO: error handling ]*/
 
-                    meerkat.probes.probes = data.body.probes;
+                        meerkat.probes.probes = data.body.probes;
 
-                    /* render the data */
-                    $('#probes').render(data.body, meerkat.probes.directives.main);
+                        /* render the data */
+                        $('#probes').render(data.body, meerkat.probes.directives.main);
 
-                    for (var p in data.body.probes) {
-                        meerkat.probes.renderProbe(p);
+                        for (var p in data.body.probes) {
+                            meerkat.probes.renderProbe(p);
+                        }
+                        meerkat.loading.off();
                     }
                 });
             },
             refreshProbe: function(p) {
-                $.getJSON(meerkat.probes.probes[p].id + '.json', function(data, textStatus, jqXHR) {
-                    /*[TODO: error handling ]*/
+                $.ajax({
+                    url: meerkat.probes.probes[p].id + '.json',
+                    dataType: 'json',
+                    error: function(jqXHR, textStatus, errorThrown) {
+                        /*[TODO: error handling ]*/
+                        alert(errorThrown);
+                        meerkat.loading.off();
+                    },
+                    success: function(data, textStatus, jqXHR) {
+                        meerkat.probes.probes[p] = data.body;
 
-                    meerkat.probes.probes[p] = data.body;
-
-                    /* render the data */
-                    meerkat.probes.renderProbe(p);
+                        /* render the data */
+                        meerkat.probes.renderProbe(p);
+                        meerkat.loading.off();
+                    }
                 });
             },
             renderProbe: function(p) {
                 var probeHtml = $('#' + meerkat.probes.probes[p].id);
+
+                /* render status */
+                probeHtml
+                    .find('.probe-body')
+                    .render(meerkat.probes.probes[p], meerkat.probes.directives.single);
+
+                /* render data */
+                var data = meerkat.probes.probes[p].data.data;
+                for (var r in data) {
+                    data[r].image_path = '<a href="' + data[r].image_path + '">' + data[r].image_path + '</a>';
+                }
+                probeHtml
+                    .find('.dbody')
+                    .html(ConvertJsonToTable(data, null, 'table', null));
 
                 /* render filters */
                 probeHtml
@@ -147,6 +232,11 @@ var meerkat = (function($) {
                     .find('.probeToggle')
                     .unbind('click')
                     .bind('click', {p: p, id: meerkat.probes.probes[p].id}, meerkat.probes.probeToggle);
+
+                probeHtml
+                    .find('.probeRefresh')
+                    .unbind('click')
+                    .bind('click', {p: p, id: meerkat.probes.probes[p].id}, meerkat.probes.probeRefresh);
 
                 /* visual aids */
                 if (meerkat.probes.probes[p].status == 'ON') {
@@ -184,10 +274,12 @@ var meerkat = (function($) {
                     '.probe': {
                         'probe<-probes': {
                             '@id': 'probe.id',
-                            'h3 span.id': 'probe.label',
-                            'dd.status': 'probe.status'
+                            'h3 span.probe-label': 'probe.label',
                         }
                     }
+                },
+                single: {
+                    'dd.status': 'status'
                 },
                 filters: {
                     'li': {
@@ -207,15 +299,26 @@ var meerkat = (function($) {
         },
         log: {
             init: function() {
-                $.getJSON('log.json', function(data, textStatus, jqXHR) {
-                    /*[TODO: error handling ]*/
-                    data.body.log = data.body.log.join("\n");
-                    $('#log').render(data.body, meerkat.log.directives.main);
+                meerkat.loading.on();
+                $.ajax({
+                    url: 'log.json',
+                    dataType: 'json',
+                    error: function(data, textStatus, jqXHR) {
+                        /*[TODO: error handling ]*/
+                        alert(errorThrown);
+                        meerkat.loading.off();
+                    },
+                    success: function(data, textStatus, jqXHR) {
+                        /*[TODO: error handling ]*/
+                        data.body.log = data.body.log.join("\n");
+                        $('#log').render(data.body, meerkat.log.directives.main);
+                        meerkat.loading.off();
+                    }
                 });
             },
             directives: {
                 main: {
-                    "pre": "log"
+                    'pre': 'log'
                 }
             }
         },
