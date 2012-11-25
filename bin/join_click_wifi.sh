a5d5627d382305def89b806338803780cb3cfc16
konker <konker@gmail.com>
2012-11-25 21:53:22 +0200

    Improve join_click_wifi glue script

--- bin/join_click_wifi.sh
+++ bin/join_click_wifi.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+PHANTOMJS=`which phantomjs`
+if [ -z "$PHANTOMJS" ]; then
+    export DISPLAY=:0
+    PHANTOMJS="pyphantomjs"
+else
+    PHANTOMJS="phantomjs"
+fi
+
 #OUT=`DISPLAY=:0 pyphantomjs bin/join_click_wifi.js`
-OUT=`phantomjs bin/join_click_wifi.js`
-if [[ $OUT == "http://*" ]]; then
+OUT=`$PHANTOMJS bin/join_click_wifi.js`
+if [[ $OUT == http:* ]]; then
+    echo $OUT
     wget -q -O - $OUT
     exec $0
 else
